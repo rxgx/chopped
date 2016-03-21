84176dce7298e97be6a6707681be0b30752df638
Ryan Gasparini <ryan@rxgx.com>
2016-03-20 17:44:22 -0700

    Simplified the webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,32 +1,25 @@
 const webpack = require('webpack');
 
-const entry = {
-  'js/app': './src/app.js'
-};
-
-const config = {
-  loaders: {
-    exclude: /node_modules/,
-    loader: 'babel',
-    test: /\.js$/
-  }
-};
-
-const output = {
-  filename: '[name].js',
-  path: './public/js'
-};
-
 const plugins = [
-  new webpack.DefinePlugin({
-    'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
-  }),
   new webpack.NoErrorsPlugin()
 ];
 
 module.exports = {
-  entry,
-  module: config,
-  output,
+  entry: {
+    app: './src/index.js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/
+      }
+    ]
+  },
+  output: {
+    filename: '[name].js',
+    path: './public/js'
+  },
   plugins
 };
