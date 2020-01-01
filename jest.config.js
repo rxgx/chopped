a0f99762d6e03ac4a6b5ea3c46f7d47b92a49552
Ryan Gasparini <ryan.gasparini@nike.com>
2020-01-01 01:26:47 -0700

    Use NextJS and move files to components and pages

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,14 @@
 module.exports = {
-  collectCoverage: true,
-  coverageDirectory: "coverage",
+  bail: true,
+  moduleNameMapper: {
+    "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "<rootDir>/test/fileTransform.js",
+    "\\.(css|less)$": "<rootDir>/test/styleTransform.js"
+  },
+  testMatch: [
+    "<rootDir>/src/**/*.(test|spec).(js|jsx)"
+  ],
+  // setupFiles: ['<rootDir>/jest.setup.js'],
+  testPathIgnorePatterns: [
+    '<rootDir>/.next/', '<rootDir>/node_modules/'
+  ],
 }
