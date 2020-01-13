74544143e54aa351e9d0ef3d900ee34e9c11cf1e
Ryan Gasparini <ryan@rxgx.com>
2020-01-13 00:15:27 -0800

    isolate the page tests

--- src/__tests__/Index.test.jsx
+++ src/__tests__/Index.test.jsx
@@ -1,6 +1,6 @@
 import { render } from '@testing-library/react';
 import React from 'react';
-import Index from './Index';
+import Index from '../pages/Index';
 
 describe('Index', () => {
   it('renders', () => {
