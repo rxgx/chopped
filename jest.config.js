08d098c49ea6ea21db5100e58068e0fdcdc25ae2
Ryan Gasparini <ryan@rxgx.com>
2020-01-12 23:45:13 -0800

    remove comment

--- jest.config.js
+++ jest.config.js
@@ -7,7 +7,6 @@ module.exports = {
   testMatch: [
     "<rootDir>/src/**/*.(test|spec).(js|jsx)"
   ],
-  // setupFiles: ['<rootDir>/jest.setup.js'],
   testPathIgnorePatterns: [
     '<rootDir>/.next/', '<rootDir>/node_modules/'
   ],
