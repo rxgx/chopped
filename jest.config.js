85587b78c026bf8f25262fc7b15e40c720e25cc7
Ryan Gasparini <ryan@rxgx.com>
2020-01-13 00:17:37 -0800

    Added setup config comment

--- jest.config.js
+++ jest.config.js
@@ -4,6 +4,7 @@ module.exports = {
     "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "<rootDir>/test/fileTransform.js",
     "\\.(css|less)$": "<rootDir>/test/styleTransform.js"
   },
+  // setupFiles: ['<rootDir>/jest.setup.js'],
   testMatch: [
     "<rootDir>/src/**/*.(test|spec).(js|jsx)"
   ],
