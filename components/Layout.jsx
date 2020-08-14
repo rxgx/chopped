87cb5543a1e90b3f12a768479869001a2b3bea7f
Ryan Gasparini <ryan@rxgx.com>
2020-08-14 00:56:53 -0700

    switch to css modules

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,21 +1,20 @@
 import React from 'react';
-import HeaderStyled from '../components/HeaderStyled';
-import LayoutStyled from '../components/LayoutStyled';
+import styles from './Layout.module.css';
 
 export default function Layout(props) {
   return (
-    <LayoutStyled>
-      <HeaderStyled>
-        <h1 className="heading">
-          <strong className="word">This</strong>
-          <em className="separator">for</em>
-          <strong className="word">That</strong>
+    <div className={styles.layout}>
+      <header>
+        <h1 className={styles.heading}>
+          <strong className={styles.word}>This</strong>
+          <em className={styles.separator}>for</em>
+          <strong className={styles.word}>That</strong>
         </h1>
-        <p className="intro">
+        <p className={styles.intro}>
           Do you have a second to spare?<br />Let me tell you my website idea.
         </p>
-      </HeaderStyled>
+      </header>
       {props.children}
-    </LayoutStyled>
+    </div>
   );
 }
--- jest.config.js
+++ jest.config.js
@@ -1,14 +1,6 @@
 module.exports = {
   bail: true,
   moduleNameMapper: {
-    "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "<rootDir>/test/fileTransform.js",
-    "\\.(css|less)$": "<rootDir>/test/styleTransform.js"
-  },
-  // setupFiles: ['<rootDir>/jest.setup.js'],
-  testMatch: [
-    "<rootDir>/src/**/*.(test|spec).(js|jsx)"
-  ],
-  testPathIgnorePatterns: [
-    '<rootDir>/.next/', '<rootDir>/node_modules/'
-  ],
+    "\\.(scss|sass|css)$": "identity-obj-proxy"
+  }
 }
