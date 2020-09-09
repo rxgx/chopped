1cfb6779f91ffebb1c37eef54c0f7160bcaaf965
Ryan Gasparini <ryan@rxgx.com>
2020-09-09 13:14:01 -0700

    change button label

--- components/Chopped.jsx
+++ components/Chopped.jsx
@@ -9,7 +9,7 @@ export function getRandomIngredients (items) {
   for (let i = 0; i < 3; i++) {
     key = keys[Math.floor(Math.random() * keys.length)]
     basket.push(items[key])
-    keys = keys.filter(item => item !== key)
+    keys = keys.filter((item) => item !== key)
     if (!keys.length) break
   }
 
@@ -28,7 +28,11 @@ export default function Chopped (props) {
     <div className={styles.container}>
       <h1>Your Entrée Basket:</h1>
       <ul>
-        {basket.map(item => <li key={item.label} className={styles.ingredient}>{item.label}</li>)}
+        {basket.map((item) => (
+          <li key={item.label} className={styles.ingredient}>
+            {item.label}
+          </li>
+        ))}
       </ul>
       <p className={styles.content}>
         <button
@@ -36,7 +40,23 @@ export default function Chopped (props) {
           onClick={handleClickEvent}
           type='button'
         >
-          Next
+          <svg
+            xmlns='http://www.w3.org/2000/svg'
+            width='24'
+            height='24'
+            viewBox='0 0 24 24'
+            fill='none'
+            stroke='currentColor'
+            strokeWidth='2'
+            stroke-linecap='round'
+            strokeLinejoin='round'
+            className='feather feather-archive'
+          >
+            <polyline points='21 8 21 21 3 21 3 8' />
+            <rect x='1' y='3' width='22' height='5' />
+            <line x1='10' y1='12' x2='14' y2='12' />
+          </svg>
+          Open Basket
         </button>
       </p>
     </div>
