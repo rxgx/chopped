16102a9f3cd66275c777bcaef26c9dc44f1a8af9
Ryan Gasparini <ryan@rxgx.com>
2020-09-09 14:25:58 -0700

    fix typo with index page

--- __tests__/Index.test.jsx
+++ __tests__/Index.test.jsx
@@ -1,9 +1,9 @@
 import { render } from '@testing-library/react'
 import React from 'react'
-import Index from '../pages/Index'
+import HomePage from '../pages/index'
 
 describe('Index', () => {
   it('renders', () => {
-    render(<Index />)
+    render(<HomePage />)
   })
 })
