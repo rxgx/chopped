67a9b6317ddc32655e97b85bd4a0a2eefc414a46
Ryan Gasparini <ryan@rxgx.com>
2020-12-12 07:59:42 -0800

    add grid layout

--- components/Basket/Basket.jsx
+++ components/Basket/Basket.jsx
@@ -13,7 +13,7 @@ export default function Basket (props) {
   return (
     <div className={styles.container}>
       <ul>
-        {basket.map((item) => (
+        {basket.map(item => (
           <li key={item.label} className={styles.ingredient}>
             {item.label}
           </li>
@@ -25,23 +25,6 @@ export default function Basket (props) {
           onClick={handleClickEvent}
           type='button'
         >
-          <svg
-            xmlns='http://www.w3.org/2000/svg'
-            width='24'
-            height='24'
-            viewBox='0 0 24 24'
-            fill='none'
-            stroke='currentColor'
-            strokeWidth='2'
-            strokeLinecap='round'
-            strokeLinejoin='round'
-            className='feather feather-archive'
-          >
-            <polyline points='21 8 21 21 3 21 3 8' />
-            <rect x='1' y='3' width='22' height='5' />
-            <line x1='10' y1='12' x2='14' y2='12' />
-          </svg>
-          &nbsp;&nbsp;
           <span>Open Basket</span>
         </button>
       </p>
