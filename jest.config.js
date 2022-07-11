f8f477507558d8bf6179dd271ac477f967541207
Ryan Gasparini <ryan@rxgx.com>
2022-07-11 16:40:50 -0700

    chore: maybe update next and jest

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,23 @@
-module.exports = {
-  bail: true,
+const nextJest = require('next/jest')
+
+const createJestConfig = nextJest({
+  // Provide the path to your Next.js app to load next.config.js and .env files in your test environment
+  dir: './'
+})
+
+// Add any custom config to be passed to Jest
+const customJestConfig = {
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
   moduleNameMapper: {
-    '\\.(css)$': 'identity-obj-proxy'
+    '\\.(css)$': 'identity-obj-proxy',
+
+    // Handle module aliases (this will be automatically configured for you soon)
+    '^@/components/(.*)$': '<rootDir>/components/$1',
+
+    '^@/pages/(.*)$': '<rootDir>/pages/$1'
   },
-  testEnvironment: 'jsdom',
-  setupFilesAfterEnv: ['<rootDir>/jest.setup.js']
+  testEnvironment: 'jest-environment-jsdom'
 }
+
+// createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
+module.exports = createJestConfig(customJestConfig)
