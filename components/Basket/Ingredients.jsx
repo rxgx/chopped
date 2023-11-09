87894bad09a76e5c99a31c6c6d5e76964135ecad
Ryan Gasparini <ryan@rxgx.com>
2023-11-09 06:07:33 +0000

    allow an empty basket

--- components/Basket/Ingredients.jsx
+++ components/Basket/Ingredients.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import styles from './Ingredients.module.css'
 
 export default function Ingredients ({ basket }) {
+    if (!basket) return null
     return (
         <ul>
         {basket.map(item => (
